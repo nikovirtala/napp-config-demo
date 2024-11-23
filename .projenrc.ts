258846ade2b74f887bb978f8a6de17efffca26da
Niko Virtala <niko.virtala@hey.com>
2024-11-23 15:40:55 +0200

    chore: enable auto-approval for deps updates

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,9 +1,19 @@
 import { AwsCdkApp } from "@nikovirtala/projen-aws-cdk-app";
+
 const project = new AwsCdkApp({
+    autoApproveOptions: {
+        allowedUsernames: ["nikovirtala"],
+        secret: "GITHUB_TOKEN",
+    },
     cdkVersion: "2.170.0",
     copyrightOwner: "Niko Virtala",
     defaultReleaseBranch: "main",
     deps: ["@aws-sdk/client-dynamodb", "@aws-sdk/util-dynamodb", "@types/aws-lambda"],
+    depsUpgradeOptions: {
+        workflowOptions: {
+            labels: ["auto-approve", "auto-merge"],
+        },
+    },
     devDeps: ["@nikovirtala/projen-aws-cdk-app"],
     license: "MIT",
     name: "napp-config-demo",
