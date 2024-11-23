45d78e207fb9143af1f81f64b091d6e1cdcec790
Niko Virtala <niko.virtala@hey.com>
2024-11-23 15:31:09 +0200

    chore(deps): update dependencies

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,34 +1,12 @@
 import { AwsCdkApp } from "@nikovirtala/projen-aws-cdk-app";
-import { javascript } from "projen";
 const project = new AwsCdkApp({
-    cdkVersion: "2.167.1",
+    cdkVersion: "2.170.0",
     copyrightOwner: "Niko Virtala",
     defaultReleaseBranch: "main",
     deps: ["@aws-sdk/client-dynamodb", "@aws-sdk/util-dynamodb", "@types/aws-lambda"],
     devDeps: ["@nikovirtala/projen-aws-cdk-app"],
-    jest: false,
     license: "MIT",
     name: "napp-config-demo",
-    prettier: true,
-    prettierOptions: {
-        settings: {
-            printWidth: 120,
-            tabWidth: 4,
-            trailingComma: javascript.TrailingComma.ALL,
-        },
-    },
-    projenrcTs: true,
-});
-
-project.vscode?.extensions.addRecommendations("dbaeumer.vscode-eslint", "esbenp.prettier-vscode");
-
-project.vscode?.settings.addSettings({
-    "editor.codeActionsOnSave": {
-        "source.fixAll": "explicit",
-    },
-    "editor.defaultFormatter": "esbenp.prettier-vscode",
-    "editor.formatOnSave": true,
-    "editor.tabSize": 4,
 });
 
 project.synth();
